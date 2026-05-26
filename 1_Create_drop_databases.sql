c7def4b385a21002d6ffbb5789fe5b101fbcc89c
Hans Carlos Cunha <hans@192.168.0.17>
2026-05-26 17:47:26 -0300

    primeiro commit

---
+++ 1_Create_drop_databases.sql
@@ -0,0 +1,5 @@
+--              nome do banco de dados criado
+CREATE DATABASE um_database_qualquer; --cria um banco de dados
+
+--            nome do banco de dados deletato
+DROP DATABASE um_database_qualquer; --deleta um banco de dados; tem que estar desconectado do database
