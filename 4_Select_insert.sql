4308da56863661f249d9e327ba2517eee71556e3
Hans Carlos Cunha <hans@192.168.0.17>
2026-05-26 19:48:05 -0300

    4

---
+++ 4_Select_insert.sql
@@ -0,0 +1,14 @@
+--busca em todas as colunas de usuarios
+SELECT * FROM usuarios;
+
+--insira à usuarios o valor tal
+INSERT INTO usuarios(nome, email) values
+	('chuchu', 'chuchusinho@gmail.com')
+	('abobora', 'abobrinha@gmail.com'),
+	('abacate', 'abacatinho@gmail.com');
+
+--busca em todos os casos onde 'A' ou 'a' é a primeira letra do nome
+SELECT * FROM usuarios WHERE nome LIKE 'A%' OR 'a%';
+
+--busca em todos os casos que o nome é chuchu
+SELECT * FROM usuarios WHERE nome = 'chuchu';
