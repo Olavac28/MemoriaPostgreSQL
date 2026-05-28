c389bdba5dd6609d81ea7c687116bf0851d18b6f
Hans <hans28h@gmail.com>
2026-05-28 09:52:16 -0300

    Modify 'usuarios' table structure
    Removed the column 'idade' and added commands to rename the table and a column.

--- 7_Alter_table.sql
+++ 7_Alter_table.sql
@@ -8,3 +8,11 @@ ADD COLUMN idade SMALLINT NOT NULL DEFAULT 0;
 ALTER TABLE usuarios
 --rertira a coluna idade
 DROP COLUMN idade;
+
+ALTER TABLE usuarios
+--muda o nome de uma tabela
+RENAME TO algum_nome;
+
+ALTER TABLE usuarios
+--renomeia uma coluna
+RENAME COLUMN nome_antigo TO nome_novo;
